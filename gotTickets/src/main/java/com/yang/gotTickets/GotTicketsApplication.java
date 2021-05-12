3be4ace57b86c3953d90273d4b8a97521b98ed21
follow1123 <59528946+follow1123@users.noreply.github.com>
2021-05-12 18:13:23 +0800

    Add files via upload

---
+++ gotTickets/src/main/java/com/yang/gotTickets/GotTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.yang.gotTickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotTicketsApplication.class, args);
+	}
+
+}
